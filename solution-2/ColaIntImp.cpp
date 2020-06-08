343f53c3024bc266f5c6909bdd22be3b43ef6eb3
Nicolás Marino <nicolasmarino.17@gmail.com>
2020-06-08 17:40:46 -0300

    Ejercicio 3, ColaIntImp

--- solution-2/ColaIntImp.cpp
+++ solution-2/ColaIntImp.cpp
@@ -2,45 +2,92 @@
 
 #ifdef COLA_INT_IMP
 
+struct NodoColaInt {
+	int dato;
+	NodoColaInt* sig;
+};
+
 struct _cabezalColaInt {
-	// NO IMPLEMENTADO
+	int cantidadPresentes;
+	NodoColaInt* primero;
+	NodoColaInt* ultimo;
 };
 
 ColaInt crearColaInt() {
-	// NO IMPLEMENTADO
-	return NULL;
+	ColaInt nuevaC = new _cabezalColaInt();
+	nuevaC->cantidadPresentes = -1;
+	nuevaC->primero = nullptr;
+	nuevaC->ultimo = nullptr;
+	return nuevaC;
+};
+
+void insertarYQuedarmeUltimoNodo(NodoColaInt*& listaRetorno, NodoColaInt*& lista, NodoColaInt*& ultimo)
+{
+	if (listaRetorno == nullptr) {
+		listaRetorno = new NodoColaInt();
+		listaRetorno->dato = lista->dato;
+		listaRetorno->sig = nullptr;
+		ultimo = listaRetorno;
+	}
+	else
+	{
+		NodoColaInt* aux = new NodoColaInt();
+		aux->dato = lista->dato;
+		aux->sig = nullptr;
+		ultimo->sig = aux;
+		ultimo = aux;
+	}
+	lista = lista->sig;
 }
 
 void encolar(ColaInt& c, int e) {
-	// NO IMPLEMENTADO
+	NodoColaInt* nuevoNodo = new NodoColaInt();
+	nuevoNodo->dato = e;
+	nuevoNodo->sig = nullptr;
+	insertarYQuedarmeUltimoNodo(c->primero, nuevoNodo, c->ultimo);
+	c->cantidadPresentes++;
 }
 
 int principio(ColaInt c) {
-	// NO IMPLEMENTADO
-	return 0;
+	assert(!esVacia(c));
+	return c->primero->dato;
 }
 
 void desencolar(ColaInt& c) {
-	// NO IMPLEMENTADO
+	c->primero = c->primero->sig;
+	c->cantidadPresentes--;
+	if (c->cantidadPresentes == -1) c->ultimo = nullptr;
 }
 
 bool esVacia(ColaInt c) {
-	// NO IMPLEMENTADO
-	return true;
+	return c->cantidadPresentes == -1;
 }
 
 unsigned int cantidadElementos(ColaInt c) {
-	// NO IMPLEMENTADO
-	return 0;
+	return c->cantidadPresentes + 1;
 }
 
 ColaInt clon(ColaInt c) {
-	// NO IMPLEMENTADO
-	return NULL;
+	ColaInt colaClon = new _cabezalColaInt();
+	colaClon->cantidadPresentes = c->cantidadPresentes;
+
+	NodoColaInt* listaRetorno = nullptr;
+	NodoColaInt* ultimo = nullptr;
+	NodoColaInt* aux = c->primero;
+	while (aux != nullptr)
+	{
+		insertarYQuedarmeUltimoNodo(listaRetorno, aux, ultimo);
+	}
+	colaClon->primero = listaRetorno;
+	colaClon->ultimo = ultimo;
+	
+	return colaClon;
 }
 
 void destruir(ColaInt& c) {
-	// NO IMPLEMENTADO
+	delete c->primero;
+	delete c->ultimo;
+	delete c;
 }
 
 #endif
