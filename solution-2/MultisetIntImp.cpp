410c95da657c46f608ee39c3e40f3133cbaee5f9
Nicolás Marino <nicolasmarino.17@gmail.com>
2020-06-30 00:43:43 -0300

    Ejercicio 10. MultisetInt

--- solution-2/MultisetIntImp.cpp
+++ solution-2/MultisetIntImp.cpp
@@ -4,76 +4,61 @@
 
 struct _cabezalMultisetInt {
 	int cantElementos;
-	NodoListaIntDobleDato* multiLista;
+	NodoListaInt* multiLista;
 };
 
 MultisetInt crearMultisetInt() {
 	MultisetInt multiset = new _cabezalMultisetInt();
-	multiset->multiLista = new NodoListaIntDobleDato();
+	multiset->multiLista = nullptr;
 	return multiset;
 }
 
-void agregarElem(NodoListaIntDobleDato* lista, int dato) {
-	NodoListaIntDobleDato* nuevo = new NodoListaIntDobleDato(dato,1);
-	lista->sig = nuevo;
-}
 
-void agregar(MultisetInt& s, int e, unsigned int ocurrencias){
-	NodoListaIntDobleDato* aRecorrer = s->multiLista;
+void agregar(MultisetInt& s, int e, unsigned int ocurrencias) {
 	bool agregue = false;
-	NodoListaIntDobleDato* nuevo = new NodoListaIntDobleDato(e, ocurrencias);
-	if (esVacio(s)) {
-		s->multiLista = nuevo;
-		s->cantElementos++; 
-		agregue = true;
-	}
-	while (aRecorrer != nullptr && agregue == false)
+
+	NodoListaInt* aRecorrer = s->multiLista;
+	while (ocurrencias != 0 && agregue == false)
 	{
-		if (aRecorrer->dato1 == e) {
-			aRecorrer->dato2 += ocurrencias;
-			agregue = true;
-		}
-		if (aRecorrer->sig == nullptr) {
-			aRecorrer->sig = nuevo;
-			s->cantElementos++;
-			agregue = true;
-		}
-		aRecorrer = aRecorrer->sig;
+		NodoListaInt* nuevo2 = new NodoListaInt(e);
+		nuevo2->sig = s->multiLista;
+		s->multiLista = nuevo2;
+		ocurrencias--;
+		s->cantElementos++;
 	}
-	
 }
 
 void borrar(MultisetInt& s, int e) {
 	bool borre = false;
-	while (s->multiLista != nullptr && s->multiLista->dato1 == e && borre==false)
+	while (s->multiLista != nullptr && s->multiLista->dato == e && borre == false)
 	{
-		NodoListaIntDobleDato* aBorrar = s->multiLista;
+		NodoListaInt* aBorrar = s->multiLista;
 		s->multiLista = s->multiLista->sig;
 		delete aBorrar;
 		s->cantElementos--;
 		borre = true;
 	}
-	NodoListaIntDobleDato* aRecorrer = s->multiLista;
+	NodoListaInt* aRecorrer = s->multiLista;
 
 	while (aRecorrer != nullptr && borre == false)
 	{
-		if (aRecorrer->sig != nullptr && aRecorrer->sig->dato1 == e) {
-			NodoListaIntDobleDato* aBorrar = aRecorrer->sig;
+		if (aRecorrer->sig != nullptr && aRecorrer->sig->dato == e) {
+			NodoListaInt* aBorrar = aRecorrer->sig;
 			aRecorrer->sig = aRecorrer->sig->sig;
 			delete aBorrar;
 			s->cantElementos--;
 			borre = true;
 		}
-		
+
 		aRecorrer = aRecorrer->sig;
 	}
 
 }
 
 bool pertenece(MultisetInt s, int e) {
-	NodoListaIntDobleDato* aRecorrer = s->multiLista;
+	NodoListaInt* aRecorrer = s->multiLista;
 	while (aRecorrer != nullptr) {
-		if (aRecorrer->dato1 == e) {
+		if (aRecorrer->dato == e) {
 			return true;
 		}
 		aRecorrer = aRecorrer->sig;
@@ -81,25 +66,22 @@ bool pertenece(MultisetInt s, int e) {
 	return false;
 }
 
-void listaOrdenadaSelectionSort(NodoListaIntDobleDato*& l)
+void listaOrdenadaSelectionSort(NodoListaInt*& l)
 {
 	if (l != nullptr) {
-		NodoListaIntDobleDato* menor = l;
-		NodoListaIntDobleDato* aux = l;
+		NodoListaInt* menor = l;
+		NodoListaInt* aux = l;
 
 		while (aux != NULL) {
-			if (aux->dato1 < menor->dato1) {
+			if (aux->dato < menor->dato) {
 				menor = aux;
 			}
 			aux = aux->sig;
 		}
 
-		int datoAux1 = l->dato1;
-		int datoAux2 = l->dato2;
-		l->dato1 = menor->dato1;
-		l->dato2 = menor->dato2;
-		menor->dato1 = datoAux1;
-		menor->dato2 = datoAux2;
+		int datoAux1 = l->dato;
+		l->dato = menor->dato;
+		menor->dato = datoAux1;
 
 		listaOrdenadaSelectionSort(l->sig);
 	}
@@ -111,6 +93,28 @@ int obtenerMenor(int uno, int dos) {
 	else {
 		return dos;
 	}
+}
+
+int obtenerMayor(int uno, int dos) {
+	if (uno > dos) {
+		return uno;
+	}
+	else {
+		return dos;
+	}
+}
+
+
+int obtenerOcurrencias(MultisetInt s, int e) {
+	NodoListaInt* aRecorrer = s->multiLista;
+	int contador = 0;
+	while (aRecorrer != nullptr) {
+		if (aRecorrer->dato == e) {
+			contador++;
+		}
+		aRecorrer = aRecorrer->sig;
+	}
+	return contador;
 }
 
 // PRE: -
@@ -125,52 +129,71 @@ MultisetInt unionConjuntos(MultisetInt s1, MultisetInt s2) {
 	MultisetInt aRetornar = crearMultisetInt();
 	while (recorrerS1->multiLista != nullptr && recorrerS2->multiLista != nullptr)
 	{
-		if (recorrerS1->multiLista->dato1 == recorrerS2->multiLista->dato1) {
-			agregar(aRetornar, recorrerS1->multiLista->dato1, recorrerS1->multiLista->dato2);
-			agregar(aRetornar, recorrerS2->multiLista->dato1, recorrerS2->multiLista->dato2);
-			recorrerS1->multiLista = recorrerS1->multiLista->sig;
-			recorrerS2->multiLista = recorrerS2->multiLista->sig;
+		if (recorrerS1->multiLista->dato == recorrerS2->multiLista->dato) {
+			int dato = recorrerS1->multiLista->dato;
+			MultisetInt nuevoRecorrer = recorrerS1;
+			MultisetInt nuevoRecorrer2 = recorrerS2;
+			int mayor = obtenerMayor(obtenerOcurrencias(nuevoRecorrer, dato), obtenerOcurrencias(nuevoRecorrer2, dato));
+			
+			while (mayor != 0)
+			{
+				agregar(aRetornar, recorrerS1->multiLista->dato, 1);
+				mayor--;
+			}
+			while (recorrerS1->multiLista != nullptr && recorrerS1->multiLista->dato == dato)
+			{
+				recorrerS1->multiLista = recorrerS1->multiLista->sig;
+			}
+			while (recorrerS2->multiLista != nullptr && recorrerS2->multiLista->dato == dato)
+			{
+				recorrerS2->multiLista = recorrerS2->multiLista->sig;
+			}
 		}
-		else if (recorrerS1->multiLista->dato1 < recorrerS2->multiLista->dato1) {
-			agregar(aRetornar, recorrerS1->multiLista->dato1, recorrerS1->multiLista->dato2);
+		else if (recorrerS1->multiLista->dato < recorrerS2->multiLista->dato) {
+			agregar(aRetornar, recorrerS1->multiLista->dato, 1);
 			recorrerS1->multiLista = recorrerS1->multiLista->sig;
 		}
 		else {
-			agregar(aRetornar, recorrerS2->multiLista->dato1, recorrerS2->multiLista->dato2);
+			agregar(aRetornar, recorrerS2->multiLista->dato, 1);
 			recorrerS2->multiLista = recorrerS2->multiLista->sig;
 		}
-	
+
 	}
-	/*while (recorrerS1->multiLista != nullptr)
-	{
-		agregar(aRetornar, recorrerS1->multiLista->dato1, recorrerS1->multiLista->dato2);
-		recorrerS1->multiLista = recorrerS1->multiLista->sig;
-	}
-	while (recorrerS2->multiLista != nullptr)
-	{
-		agregar(aRetornar, recorrerS2->multiLista->dato1, recorrerS2->multiLista->dato2);
-		recorrerS2->multiLista = recorrerS2->multiLista->sig;
-	}*/
 	return aRetornar;
 }
 
 
 MultisetInt interseccionConjuntos(MultisetInt s1, MultisetInt s2) {
-	NodoListaIntDobleDato* recorrerS1 = clon(s1)->multiLista;
-	listaOrdenadaSelectionSort(recorrerS1);
-	NodoListaIntDobleDato* recorrerS2 = clon(s2)->multiLista;
-	listaOrdenadaSelectionSort(recorrerS2);
+	MultisetInt recorrerS1 = clon(s1);
+	listaOrdenadaSelectionSort(recorrerS1->multiLista);
+	MultisetInt recorrerS2 = clon(s2);
+	listaOrdenadaSelectionSort(recorrerS2->multiLista);
 
 	MultisetInt aRetornar = crearMultisetInt();
-	while (recorrerS1 != nullptr && recorrerS2 != nullptr)
+	while (recorrerS1->multiLista != nullptr && recorrerS2->multiLista != nullptr)
 	{
-		if (recorrerS1->dato1 < recorrerS2->dato1) recorrerS1 = recorrerS1->sig;
-		else if (recorrerS1->dato1 > recorrerS2->dato1) recorrerS2 = recorrerS2->sig;
-		else if (recorrerS1->dato1 == recorrerS2->dato1) {
-			agregar(aRetornar, recorrerS1->dato1, obtenerMenor(recorrerS1->dato2, recorrerS2->dato2));
-			recorrerS1 = recorrerS1->sig;
-			recorrerS2 = recorrerS2->sig;
-		}		
+		if (recorrerS1->multiLista->dato < recorrerS2->multiLista->dato) recorrerS1->multiLista = recorrerS1->multiLista->sig;
+		else if (recorrerS1->multiLista->dato > recorrerS2->multiLista->dato) recorrerS2->multiLista = recorrerS2->multiLista->sig;
+		else if (recorrerS1->multiLista->dato == recorrerS2->multiLista->dato) {
+			int dato = recorrerS1->multiLista->dato;
+			MultisetInt nuevoRecorrer = recorrerS1;
+			MultisetInt nuevoRecorrer2 = recorrerS2;
+			int menor = obtenerMenor(obtenerOcurrencias(nuevoRecorrer, dato), obtenerOcurrencias(nuevoRecorrer2, dato));
+
+			while (menor != 0)
+			{
+				agregar(aRetornar, recorrerS1->multiLista->dato, 1);
+				menor--;
+			}
+			while (recorrerS1->multiLista != nullptr && recorrerS1->multiLista->dato == dato)
+			{
+				recorrerS1->multiLista = recorrerS1->multiLista->sig;
+			}
+			while (recorrerS2->multiLista != nullptr && recorrerS2->multiLista->dato == dato)
+			{
+				recorrerS2->multiLista = recorrerS2->multiLista->sig;
+			}
+		}
 	}
 	return aRetornar;
 }
@@ -180,40 +203,76 @@ MultisetInt interseccionConjuntos(MultisetInt s1, MultisetInt s2) {
 //   	 o sea, el conjunto que contiene solo los elementos que estan en m1 pero no en m2.
 //   	 El conjunto retornado no debe compartir memoria con m1 ni m2.
 MultisetInt diferenciaConjuntos(MultisetInt s1, MultisetInt s2) {
-	NodoListaIntDobleDato* recorrerS1 = s1->multiLista;
+	NodoListaInt* recorrerS1 = s1->multiLista;
+
 	MultisetInt aRetornar = crearMultisetInt();
 	while (recorrerS1 != nullptr)
 	{
-		if (!pertenece(s2, recorrerS1->dato1)) {
-			agregar(aRetornar, recorrerS1->dato1, recorrerS1->dato2);
+		MultisetInt recorrers1 = s1;
+		MultisetInt recorrers2 = s2;
+		int dato = recorrerS1->dato;
+		int ocurrenciasEnS1 = obtenerOcurrencias(recorrers1, recorrerS1->dato);
+		int ocurrenciasEnS2 = obtenerOcurrencias(recorrers2, recorrerS1->dato);
+		int cantidad = ocurrenciasEnS1 - ocurrenciasEnS2;
+		if (cantidad > 0) {
+			agregar(aRetornar, recorrerS1->dato, 1);
 		}
-		recorrerS1 = recorrerS1->sig;
+		while (recorrerS1 != nullptr && recorrerS1->dato == dato)
+		{
+			recorrerS1 = recorrerS1->sig;
+		}
 	}
 	return aRetornar;
 }
 
-bool embebida(NodoListaIntDobleDato* l, NodoListaIntDobleDato* secuencia) {
-	if (l == nullptr && secuencia == nullptr) return true;
-	if (l == nullptr) return false;
-	if (secuencia == nullptr) return true;
-	if (l != nullptr && secuencia != nullptr)
-		if (l->dato1 == secuencia->dato1)
-			embebida(l->sig, secuencia->sig);
-		else
-			return false;
-}
+//bool embebida(NodoListaInt* l, NodoListaInt* secuencia) {
+//	if (l == nullptr && secuencia == nullptr) return true;
+//	if (l == nullptr) return false;
+//	if (secuencia == nullptr) return true;
+//	if (l != nullptr && secuencia != nullptr)
+//		if (l->dato == secuencia->dato) {
+//			int datoL = l->dato;
+//			while (l->dato == datoL)
+//			{
+//				l = l->sig;
+//			}
+//			while (secuencia->dato == datoL)
+//			{
+//				secuencia = secuencia->sig;
+//			}
+//			embebida(l, secuencia);
+//		}			
+//		else
+//			return false;
+//}
 //PRE: -
 //POS: Retorna true sii todos los elementos de m1 pertenecen a m2
 bool contenidoEn(MultisetInt s1, MultisetInt s2) {
-	NodoListaIntDobleDato* recorrerS1 = s1->multiLista;
-	NodoListaIntDobleDato* recorrerS2 = s2->multiLista;
-	return embebida(recorrerS1, recorrerS2);
+	NodoListaInt* recorrerS1 = s1->multiLista;
+	NodoListaInt* recorrerS2 = s2->multiLista;
+	while (recorrerS1 != nullptr)
+	{	
+		MultisetInt recorrers1 = s1;
+		MultisetInt recorrers2 = s2;
+		int dato = recorrerS1->dato;
+		int ocurrenciasEnS1 = obtenerOcurrencias(recorrers1, recorrerS1->dato);
+		int ocurrenciasEnS2 = obtenerOcurrencias(recorrers2, recorrerS1->dato);
+		if (ocurrenciasEnS1 > ocurrenciasEnS2) return false;
+		if (pertenece(recorrers2, recorrerS1->dato)) {
+			recorrerS1 = recorrerS1->sig;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	return true;
 }
 
 
 int elemento(MultisetInt s) {
 	assert(!esVacio(s));
-	return s->multiLista->dato1;
+	return s->multiLista->dato;
 }
 
 bool esVacio(MultisetInt s) {
@@ -237,11 +296,10 @@ void destruir(MultisetInt& s) {
 
 }
 
-NodoListaIntDobleDato* clonare(NodoListaIntDobleDato* lista) {
+NodoListaInt* clonare(NodoListaInt* lista) {
 	if (lista == nullptr) return nullptr;
-	NodoListaIntDobleDato* aux = new NodoListaIntDobleDato();
-	aux->dato1 = lista->dato1;
-	aux->dato2 = lista->dato2;
+	NodoListaInt* aux = new NodoListaInt();
+	aux->dato = lista->dato;
 	aux->sig = clonare(lista->sig);
 	return aux;
 }
